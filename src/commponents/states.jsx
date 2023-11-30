2dda954ca23ec8f4f94de5293d6625835d001e0e
abdelrahmanrgab <abdelrahmanragab787@gmail.com>
2023-11-30 17:07:14 +0200

    counter

--- src/commponents/states.jsx
+++ src/commponents/states.jsx
@@ -1,38 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const States = () => {
- let statistic = [
+  const statistic = [
     {
-        stateName:"Years Of Experiance",
-        stateValue:12,
+      stateName: "Years Of Experience",
+      stateValue: 12,
     },
     {
-        stateName:"Success Project",
-        stateValue:85,
+      stateName: "Success Project",
+      stateValue: 85,
     },
     {
-        stateName:"Active Project",
-        stateValue:15,
+      stateName: "Active Project",
+      stateValue: 15,
     },
     {
-        stateName:"Happy Customers",
-        stateValue:95,
+      stateName: "Happy Customers",
+      stateValue: 95,
     },
-];   
+  ];
 
+  const [started, setStarted] = useState(false);
+
+  const startCount = (el, goal) => {
+    let count = setInterval(() => {
+      el.textContent++;
+      if (parseInt(el.textContent) === goal) {
+        clearInterval(count);
+      }
+    }, 2000 / goal);
+  };
+
+  const handleScroll = () => {
+    const elements = document.querySelectorAll('.state-num');
+    if (!started) {
+      elements.forEach((el, index) => startCount(el, statistic[index].stateValue));
+      setStarted(true);
+      window.removeEventListener('scroll', handleScroll); // Remove the event listener after starting
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [started, statistic]);
 
   return (
     <div className='state'>
-    <div className='container'>
-
-    {statistic.map((data) => (     
-    <div className='state-box'>
-        <div className='state-num'>{data.stateValue}</div>
-        <div className='state-name'>{data.stateName}</div>
-      </div>  
-    ))}
-    </div>  
-    </div>  )
-}
+      <div className='container'>
+        {statistic.map((data, index) => (
+          <div className='state-box' key={index}>
+            <div className='state-num'>0</div>
+            <div className='state-name'>{data.stateName}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default States;
