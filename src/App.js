5f0150765f59dbca7b0c68f71e6b89383a32fc0f
abdelrahmanrgab <abdelrahmanragab787@gmail.com>
2023-12-14 17:02:06 +0200

    initial state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import {useSelector , useDispatch} from 'react-redux'
-import {increment , decrement , login , logout} from './store/actions'
+import "../src/assets/css/globals6.css";
 function App() {
-  const count = useSelector( state => state.counter.count);
-  const islogged = useSelector( state => state.logged.logged);
-  const dispatch = useDispatch();
-  return (
-<div className='app'>
-<header>
-<div> {count} </div>
-{islogged ?
- (
- <button onClick={() => dispatch(logout())}> logout </button>
- ) : (
-  <button onClick={() => dispatch(login())}> login </button>
-  )}
-</header>
-<div className="counter">
-<button onClick={() => dispatch(increment())}> + </button>
-<button onClick={() => dispatch(increment(3))}> + 3 </button>
-<button onClick={() => dispatch(decrement())}> - </button>
-</div>
-</div>
+<>
+</>
     );
 }
 
---
+++ src/components/sections/heros/hero6/Hero6.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./hero.css";
+import { useSelector } from "react-redux";
+
+const Hero1 = () => {
+  const { hero } = useSelector((state) => state.template);
+  return (
+    <div className="landing design-1 " style={{ backgroundImage: `url(${hero.imgUrl})` }}>
+      <div className="overlay"></div>
+      <div className="text">
+        <div className="content">
+          <h2>{hero.title}</h2>
+          <p>{hero.description}</p>
+          <button className="landing-btn">{hero.buttonText} </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero1;
