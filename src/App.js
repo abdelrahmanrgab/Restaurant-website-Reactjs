a8f0f09c00aaee6aece65008fd26a06d400db2e3
abdelrahmanrgab <abdelrahmanragab787@gmail.com>
2023-12-14 18:20:34 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "../src/assets/css/globals6.css";
+import "./assets/css/globals6.css";
 import Navbar6 from "./components/sections/navbars/navbar6/Navbar6"
 import Hero6 from "./components/sections/heros/hero6/Hero6"
 import CartItems6 from "./components/sections/CartItems/cartItems6/CartItems6"
@@ -7,6 +7,7 @@ import FilterItems6 from "./components/sections/filterItems/filterItems6/FilterI
 import Testimonials6 from "./components/sections/testimonials/testimonials6/Testimonials6"
 import Cta6 from "./components/sections/ctas/cta6/Cta6"
 import Footer6 from "./components/sections/footers/footer6/Footer6"
+
 function App() {
 return (
 <>
